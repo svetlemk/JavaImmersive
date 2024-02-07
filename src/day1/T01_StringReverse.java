c18fe4bcf053c81c60c68876381a0af5089f3068
skocherry <svetlemk@gmail.com>
2024-02-06 20:16:00 -0600

    FCH:787289 adding day 1 code reverse string using StringBuiilder

--- src/day1/T01_StringReverse.java
+++ src/day1/T01_StringReverse.java
@@ -1,4 +1,28 @@
 package day1;
-
+// using 2 method, inbuild from string builder and with loop
 public class T01_StringReverse {
+    public static void main(String[] args){
+
+        String str= "word";
+        str = new StringBuilder(str).reverse().toString();
+        System.out.println("word1 reversed:  "+ str);
+
+        String word = "apple";
+        String reverseString = "";
+
+        for (int i = word.length()-1; i >=0 ;i--){
+            reverseString += word.charAt(i);
+
+
+        }
+        for(int i= str.length()-1;i>= 0; i--){
+
+        }
+
+System.out.println("word2 reversed: "+ reverseString);
+
+        for(int i= str.length()-1;i>= 0; i--){
+
+        }
+    }
 }
